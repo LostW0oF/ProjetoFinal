4658f2f973e813f7b53eed25ab8bc9ff496869df
Sully Brito <sullyz.sb@gmail.com>
2022-05-27 15:33:13 -0300

    MAN!
    EU DEI COMIMIT

--- FinalProject/FinalProject/Program.cs
+++ FinalProject/FinalProject/Program.cs
@@ -6,7 +6,45 @@ namespace FinalProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            elevador.MostrarAndares();
+        }
+    }
+
+    public class elevador
+    {
+
+        public static void MostrarAndares()
+        {
+
+            int AndarAtual = 0;
+
+
+            Console.WriteLine(@"Qual andar deseja ir?
+            1 - Piso
+            2 - Lojas
+            3 - Lanchonetes
+            4 - Livrarias");
+
+            int AndarEscolhido = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+
+            if (AndarEscolhido <= 0 || AndarEscolhido >= 5)
+            {
+                Console.WriteLine("Numeros Invalidos");
+                MostrarAndares();
+            }
+            else
+            {
+                if(AndarEscolhido > AndarAtual)
+                {
+                    for(int i = AndarAtual; i < AndarEscolhido; i++)
+                    {
+                        Console.WriteLine($"Você está no andar {i}");
+                    }
+                }
+                MostrarAndares();
+            }
         }
     }
 }
