26b2da7dc85f2c5cf9c1bc7c372576461cc1a419
Sully Brito <sullyz.sb@gmail.com>
2022-05-29 12:55:19 -0300

    ver.1.22474487139

--- FinalProject/FinalProject/Program.cs
+++ FinalProject/FinalProject/Program.cs
@@ -15,6 +15,8 @@ namespace FinalProject
     {
         public int totalAndares = 5;
         public int andar = 0;
+        public int capacidade = 0;
+        public int capacidadeMax = 6;
         public bool continuar = true;
 
         public void Menu()
@@ -40,26 +42,46 @@ namespace FinalProject
                     4 - Cobertura
                     5 - Area de serviço
                     6 - Sair");
-            Console.WriteLine(@"Digite A para subir até ao andar desejado e B para descer e C para sair");
+            Console.WriteLine(@"Digite E para entrar, A para subir até ao andar desejado e B para descer, S para sair e F para encerrar ");
         }
 
         public void andarAtual(string Andar)
         {
             switch (Andar)
             {
+                case "E":
+                    entrar();
+                    break;
                 case "A":
                     subir();
                     break;
                 case "B":
                     descer();
                     break;
-                case "C":
+                case "S":
+                    sair();
+                    break;
+                case "F":
                     Console.WriteLine(@"Saindo do elevador");
                     continuar = false;
                     break;
                 default:
                     Console.WriteLine("Esse andar Não existe.");
                     break;
+            }
+        }
+
+
+        public void entrar()
+        {
+            if(capacidade < capacidadeMax)
+            {
+                Console.WriteLine(@"Uma pessoa entrou no elevador");
+                capacidade++;
+            }
+            else
+            {
+                Console.WriteLine(@"Elevador está cheio. Não é possível entrar");
             }
         }
 
@@ -130,6 +152,19 @@ namespace FinalProject
             else
             {
                 Console.WriteLine(@"Você já está no Estacionamento, não é possivel descer mais");
+            }
+        }
+
+        public void sair()
+        {
+            if (capacidade > 0)
+            {
+                Console.WriteLine(@"Uma pessoa saiu do elevador");
+                capacidade--;
+            }
+            else
+            {
+                Console.WriteLine(@"O elevador está vazio");
             }
         }
     }
